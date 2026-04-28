09d02b71a8f9bffedf3f709436d04b6083a9fa33
Ruoming Shen <rs3270@princeton.edu>
2026-04-27 22:04:36 -0400

    Fix get_expert_action naming inconsistency

--- src/alfworld_lfm/bc.py
+++ src/alfworld_lfm/bc.py
@@ -8,7 +8,7 @@ from base_train import BaseExpertDataset, train_imitation_learning
 class BCDataset(BaseExpertDataset):
     """BC --> uses ALFWorld's built-in expert"""
 
-    def _get_expert_actions(self, env, instruction, obs, actions, trajectory):
+    def _get_expert_action(self, env, instruction, obs, actions, trajectory):
         """Gets expert action from ALFWorld's built-in expert"""
         return env.get_expert_action()
 
